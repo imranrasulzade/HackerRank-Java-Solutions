f6cba4f92fb8eaa5d735f352ee5dc6a72b528287
imranrasulzade <imranrasulzade5@gmail.com>
2023-04-24 18:56:04 +0400

    Java (Basic) | Easy

---
+++ Java_end_of_file/src/main/java/com/mycompany/java_end_of_file/Java_end_of_file.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.java_end_of_file;
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+/**
+ *
+ * @author imran
+ */
+public class Java_end_of_file {
+
+     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int i = 0;
+        while(sc.hasNext())
+        {
+            String st = sc.nextLine();
+            i++;
+            System.out.println(i+" "+st);
+        }
+    }
+}
